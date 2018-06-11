2ad3491188fbffd669b07539095c5e5a11c1c0e6
ITP-KSA <33999656+ITP-KSA@users.noreply.github.com>
2018-06-11 12:42:07 +0300

    update
    Duplicateed

--- ITP_sale_purchase_customizations/models/account_invoice.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import api, fields, models, _
-
-
-class iSkyAccountInvoice(models.Model):
-    _inherit = "account.invoice"
-
-    client_po = fields.Char(string="Client's P.O")
--- ITP_sale_purchase_customizations/models/stock_picking.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import api, fields, models, _
-from odoo.exceptions import ValidationError
-
-class Picking(models.Model):
-    _inherit = "stock.picking"
-
-    client_po = fields.Char(string="Client's P.O")
